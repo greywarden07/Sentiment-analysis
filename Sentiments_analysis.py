306553c271968f7001a2f2f3136b4d7fb3181713
Divyansh Gautam <sadhanagautam200@gmail.com>
2020-05-06 06:56:56 +0000

    Update Sentiments_analysis.py

--- Sentiments_analysis.py
+++ Sentiments_analysis.py
@@ -1,11 +1,16 @@
 from textblob import TextBlob
 
+
+# Making the text input by the user which has to be analyse
 text = input("Enter your text which has to be analyse->")
 
 
 root = TextBlob(text)
 
 
+# The output values will be between -1 to +1
+# If the number comes as -ive value then the given text is said in negative manner
+# If the number comes as +ive value then the given text is said in positive manner
 sentiment = root.sentiment.polarity
 
 
@@ -13,6 +18,7 @@ sentiment = root.sentiment.polarity
 print(sentiment)
 
 
+# Writing the conditional statements for the answers
 if sentiment<0:
     print("Sentiment is Negative")
 
